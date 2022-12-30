efd6fafd90572ae788d7fd9a19cfbef23b3b94b2
DDeShon <36036284+DDeShon@users.noreply.github.com>
2022-12-29 23:15:35 -0600

    set up colors

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import { useState } from "react";
 
 function App() {
+  const [theme, colorMode] = useMode();
   return <div className="app"></div>;
 }
 
