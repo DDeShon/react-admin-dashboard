7eb5e0e329b209dca90b1766066df45b2effeb5c
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-01-09 23:31:01 -0600

    worked on app structure

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -14,7 +14,7 @@ const Topbar = () => {
     const colors = tokens(theme.palette.mode);
     const colorMode = useContext(ColorModeContext);
 
-    return <div>Topbar</div>
+    return (<div>Topbar</div>);
 }
 
 export default Topbar;
