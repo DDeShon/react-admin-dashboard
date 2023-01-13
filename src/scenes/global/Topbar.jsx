2a1969e16d9dbd69f70ee763f82d1e95d357c2c3
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-01-13 09:31:51 -0600

    worked on app structure

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -14,7 +14,7 @@ const Topbar = () => {
     const colors = tokens(theme.palette.mode);
     const colorMode = useContext(ColorModeContext);
 
-    return (<Box display="flex" justifyContent="space-between">
+    return (<Box display="flex" justifyContent="space-between" p={2}>
 
     </Box>);
 }
