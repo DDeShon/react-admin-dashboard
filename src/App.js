927cf53db2c9da318f220f6d4aa55ab3484b6ff3
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-01-27 08:31:43 -0600

    worked on top navbar

--- src/App.js
+++ src/App.js
@@ -30,12 +30,12 @@ function App() {
               <Route path="/team" element={<Team />} />
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/contacts" element={<Contacts />} />
-              <Route path="/" element={<Dashboard />} />
-              <Route path="/" element={<Dashboard />} />
-              <Route path="/" element={<Dashboard />} />
-              <Route path="/" element={<Dashboard />} />
-              <Route path="/" element={<Dashboard />} />
-              <Route path="/" element={<Dashboard />} />
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/form" element={<Form />} />
+              <Route path="/line" element={<Line />} />
+              <Route path="/pie" element={<Pie />} />
+              <Route path="/faq" element={<FAQ />} />
+              <Route path="/geography" element={<Geography />} />
               <Route path="/" element={<Dashboard />} />
             </Routes>
           </main>
