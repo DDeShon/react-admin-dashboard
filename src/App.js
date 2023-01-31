fb7464f807f4d19008e4124d0104187777f39638
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-01-31 08:59:35 -0600

    worked on top navbar

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
             <Topbar />
             <Routes>
               <Route path="/" element={<Dashboard />} />
-              <Route path="/team" element={<Team />} />
+              {/* <Route path="/team" element={<Team />} />
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/bar" element={<Bar />} />
@@ -37,7 +37,7 @@ function App() {
               <Route path="/pie" element={<Pie />} />
               <Route path="/faq" element={<FAQ />} />
               <Route path="/geography" element={<Geography />} />
-              <Route path="/calendar" element={<Calendar />} />
+              <Route path="/calendar" element={<Calendar />} /> */}
             </Routes>
           </main>
         </div>
