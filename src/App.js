9d3ab14b1b91e13f7e0b2d173ce9888bf7beebb8
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-01-30 22:50:18 -0600

    worked on top navbar

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ function App() {
               <Route path="/pie" element={<Pie />} />
               <Route path="/faq" element={<FAQ />} />
               <Route path="/geography" element={<Geography />} />
-              <Route path="/" element={<Dashboard />} />
+              <Route path="/calendar" element={<Calendar />} />
             </Routes>
           </main>
         </div>
