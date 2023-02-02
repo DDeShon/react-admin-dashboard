11cd08ce589d4c23cd3fc3fba5f6758f284d5499
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-02-01 23:26:58 -0600

    worked on top navbar

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
+          <Sidebar />
           <main className="content">
             <Topbar />
             <Routes>
