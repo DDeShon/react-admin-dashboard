d994e071a962fd0c8811ccb8c914be1c335cf1b0
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-02-06 23:01:24 -0600

    worked on sidebar

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { ProSidebar, Menu, MenuItem } from "react-pro-sidebar";
+import 'react-pro-sidebar/dist/css/styles.css';
+import { Box, IconButton, Typography, useTheme } from "@mui/material";
 
 const Sidebar = () => {
     return <div>Sidebar</div>
