aaf11f88ad4f13323afd8c1dc0b107af39b9e7e6
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-02-22 23:19:33 -0600

    worked on sidebar

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -75,7 +75,13 @@ const Sidebar = () => {
                     {!isCollapsed && (
                         <Box mb="25px">
                             <Box display="flex" justifyContent="center" alignContent="center">
-                                <img />
+                                <img
+                                    alt="profile-user"
+                                    width="100px"
+                                    height="100px"
+                                    src={`.../../assets/user.png`}
+                                    style={{ cursor: "pointer", borderRadius="50%"}}
+                                />
                             </Box>
 
                             <Box>
