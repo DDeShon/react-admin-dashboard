72fa663a6810f95baeaf718cebe79dc90758d635
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-03-16 02:38:47 -0500

    worked on sidebar menu items

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -21,7 +21,14 @@ const Item = ({ title, to, icon, selected, setSelected }) => {
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
     return (
-        <MenuItem active={selected === title} style={{ color: colors.grey[100] }} onClick={() => setSelected(title)} icon={icon}>
+        <MenuItem 
+            active={selected === title} 
+            style={{ 
+                color: colors.grey[100] 
+            }} 
+            onClick={() => setSelected(title)} 
+            icon={icon}
+        >
             <Typography>{title}</Typography>
             <Link to={to} />
         </MenuItem>
