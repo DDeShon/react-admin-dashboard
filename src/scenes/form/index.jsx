cdb41443c749e1e3ac57a6fcda8e1ceaff3475bf
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-03-17 02:57:25 -0500

    worked on form page

--- src/scenes/form/index.jsx
+++ src/scenes/form/index.jsx
@@ -43,7 +43,24 @@ const Form = () => {
         onSubmit={handleFormSubmit}
         initialValues={initialValues}
         validationSchema={userSchema}
-      ></Formik>
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleBlur,
+          handleChange,
+          handleSubmit,
+        }) => (
+          <form onSubmit={handleSubmit}>
+            <Box
+              display="grid"
+              gap="30px"
+              gridTemplateColumns="repeat(4, minmax(0, 1fr))"
+            ></Box>
+          </form>
+        )}
+      </Formik>
     </Box>
   );
 };
