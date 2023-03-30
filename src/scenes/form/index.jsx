b56eedbb2267bdf46aa755028dfd5ad873dbeebf
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-03-29 19:02:09 -0500

    worked on form page

--- src/scenes/form/index.jsx
+++ src/scenes/form/index.jsx
@@ -130,13 +130,13 @@ const Form = () => {
                 fullWidth
                 variant="filled"
                 type="text"
-                label="First Name"
+                label="City"
                 onBlur={handleBlur}
                 onChange={handleChange}
-                value={values.firstName}
-                name="firstName"
-                error={!!touched.firstName && !!errors.firstName}
-                helperText={touched.firstName && errors.firstName}
+                value={values.city}
+                name="city"
+                error={!!touched.city && !!errors.city}
+                helperText={touched.city && errors.city}
                 sx={{ gridColumn: "span 2" }}
               />
               <TextField
