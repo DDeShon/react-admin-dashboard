5801eda7be07ece03619b73387615ece4b5658c7
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-04-10 21:37:57 -0500

    worked on calendar page

---
+++ src/scenes/calendar/index.jsx
@@ -0,0 +1,5 @@
+import { useState } from "react";
+import FullCalendar, { formatDate } from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import listPlugin from "@fullcalendar/list";
