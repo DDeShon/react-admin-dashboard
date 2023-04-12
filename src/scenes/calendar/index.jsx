d340070f14da51ec62712844c890cb0154c5a2ea
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-04-11 22:38:31 -0500

    worked on calendar page

--- src/scenes/calendar/index.jsx
+++ src/scenes/calendar/index.jsx
@@ -2,4 +2,13 @@ import { useState } from "react";
 import FullCalendar, { formatDate } from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
 import listPlugin from "@fullcalendar/list";
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+  useTheme,
+} from "@mui/material";
