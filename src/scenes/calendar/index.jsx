a812466a27680a5866e7dcde881bcfe1abc6491b
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-04-17 22:59:23 -0500

    worked on calendar page

--- src/scenes/calendar/index.jsx
+++ src/scenes/calendar/index.jsx
@@ -89,7 +89,21 @@ const Calendar = () => {
 
         {/* Calendar */}
         <Box flex="1 1 100%" ml="15px">
-          <FullCalendar />
+          <FullCalendar
+            height="75vh"
+            plugins={[
+              dayGridPlugin,
+              timeGridPlugin,
+              interactionPlugin,
+              listPlugin,
+            ]}
+            headerToolbar={{
+              left: "prev, next, today",
+              center: "title",
+              right: "dayGridMonth, timeGridWeek, timeGridDay, listMonth",
+            }}
+            initialView="dayGridMonth"
+          />
         </Box>
       </Box>
     </Box>
