9a200752ebbc9ffd54ed3702b795638f783e0735
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-04-25 22:13:12 -0500

    worked on bar chart

--- src/components/BarChart.jsx
+++ src/components/BarChart.jsx
@@ -1,6 +1,7 @@
 import { useTheme } from "@mui/material";
 import { ResponsiveBar } from "@nivo/bar";
 import { tokens } from "../theme";
+import { mockBarData as data } from "../data/mockData";
 
 const BarChart = () => {
   return (
