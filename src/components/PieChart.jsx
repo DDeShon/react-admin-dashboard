5e368f718f643eb13c7cab57ace9f9cfffd00c39
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-05-02 23:08:37 -0500

    worked on pie chart

--- src/components/PieChart.jsx
+++ src/components/PieChart.jsx
@@ -4,6 +4,9 @@ import { tokens } from "../theme";
 import { mockPieData as data } from "../data/mockData";
 
 const PieChart = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
   return (
     <ResponsivePie
       data={data}
