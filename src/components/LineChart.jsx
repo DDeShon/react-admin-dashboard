3e55878bf46874a322220c9950c2cf622139c2a2
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-05-08 21:52:55 -0500

    worked on line chart

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -3,7 +3,7 @@ import { ResponsiveLine } from "@nivo/line";
 import { tokens } from "../theme";
 import { mockLineData as data } from "../data/mockData";
 
-const LineChart = () => {
+const LineChart = ({ isDashboard = false }) => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
 
@@ -43,6 +43,7 @@ const LineChart = () => {
           },
         },
       }}
+      colors={isDashboard ? { datum: "color" } : { scheme: "nivo" }}
       margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
       xScale={{ type: "point" }}
       yScale={{
