8bc95688121d3702492959728b070a28285ca6a3
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-05-15 10:17:26 -0500

    worked on geography map

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import FAQ from "./scenes/faq";
 // import Bar from "./scenes/bar";
 import Line from "./scenes/line";
 import Pie from "./scenes/pie";
-// import Geography from "./scenes/geography";
+import Geography from "./scenes/geography";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -37,7 +37,7 @@ function App() {
               {/* <Route path="/bar" element={<Bar />} /> */}
               <Route path="/line" element={<Line />} />
               <Route path="/pie" element={<Pie />} />
-              {/* <Route path="/geography" element={<Geography />} /> */}
+              <Route path="/geography" element={<Geography />} />
             </Routes>
           </main>
         </div>
