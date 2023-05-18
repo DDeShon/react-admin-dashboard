af238b64e8b804aa8f09c828a534b24cb0f4ebce
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-05-17 19:14:09 -0500

    worked on progress rings

--- src/components/ProgressCircle.jsx
+++ src/components/ProgressCircle.jsx
@@ -5,6 +5,16 @@ const ProgressCircle = ({ progress = "0.75", size = "40" }) => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   const angle = progress * 360;
+
+  return (
+    <Box
+      sx={{
+        background: `radial-gradient(${colors.primary[400]} 55%, transparent 56%)
+        conic-tradient(transparent 0deg ${angle}deg, ${colors.blueAccent[500]} ${angle}deg 360deg),
+      `,
+      }}
+    />
+  );
 };
 
 export default ProgressCircle;
