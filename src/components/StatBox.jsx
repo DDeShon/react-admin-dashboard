d00a399058f8e33dea6aeae5bfe77091c2213b78
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-05-21 22:54:57 -0500

    worked on dashboard

--- src/components/StatBox.jsx
+++ src/components/StatBox.jsx
@@ -19,27 +19,26 @@ const StatBox = ({ title, subtitle, icon, progress, increase }) => {
             {title}
           </Typography>
         </Box>
-
         <Box>
           <ProgressCircle progress={progress} />
         </Box>
+      </Box>
 
-        <Box display="flex" justifyContent="space-between">
-          <Typography variant="h5" sx={{ color: colors.greenAccent[500] }}>
-            {subtitle}
-          </Typography>
-        </Box>
+      <Box display="flex" justifyContent="space-between">
+        <Typography variant="h5" sx={{ color: colors.greenAccent[500] }}>
+          {subtitle}
+        </Typography>
+      </Box>
 
-        <Box>
-          {icon}
-          <Typography
-            variant="h5"
-            fontStyle="italic"
-            sx={{ color: colors.greenAccent[600] }}
-          >
-            {increase}
-          </Typography>
-        </Box>
+      <Box>
+        {icon}
+        <Typography
+          variant="h5"
+          fontStyle="italic"
+          sx={{ color: colors.greenAccent[600] }}
+        >
+          {increase}
+        </Typography>
       </Box>
     </Box>
   );
