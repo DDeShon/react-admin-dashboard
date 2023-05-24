77a6fd69c9951779938df2ffcbaaf1fdd824988a
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-05-23 23:30:34 -0500

    worked on dashboard

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -179,7 +179,38 @@ const Dashboard = () => {
               borderBottom={`4px solid ${colors.primary[500]}`}
               colors={colors.grey[100]}
               p="15px"
-            ></Box>
+            >
+              <Typography
+                color={colors.grey[100]}
+                variant="h5"
+                fontWeight="600"
+              >
+                Recent Transactions
+              </Typography>
+            </Box>
+            {mockTransactions.map((transaction, i) => (
+              <Box
+                key={`${transaction.txId}-${i}`}
+                display="flex"
+                justifyContent="space-between"
+                alignItems="center"
+                borderBottom={`4px solid ${colors.primary[500]}`}
+                p="15px"
+              >
+                <Box>
+                  <Typography
+                    color={colors.greenAccent[500]}
+                    variant="h5"
+                    fontWeight="600"
+                  >
+                    {transaction.txId}
+                  </Typography>
+                  <Typography color={colors.grey[100]}>
+                    {transaction.user}
+                  </Typography>
+                </Box>
+              </Box>
+            ))}
           </Box>
         </Box>
       </Box>
